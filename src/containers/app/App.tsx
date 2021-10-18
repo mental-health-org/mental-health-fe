0e5f51d9fb62b639995c2bd39a3ec30b4dd1e93f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 17:45:54 -0600

    Add useEffect and dummy data for tags

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
 import { Route, Link, Switch } from "react-router-dom";
+import LandingPage from '../landingPage/LandingPage';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [tags, setTags] = useState<string[]>(['tag1', 'tag2', 'tag3']);
+
+  useEffect(() => {
+    //fetch all tags
+    //fetch all posts
+
+  }, [])
+
   return (
     <div className="App">
       {/* <Switch> */}
       <Route
           exact
           path="/"
-          render={() => <p>test</p>} 
+          // past tags and posts to landing page
+          render={() => <LandingPage tags={tags}/>} 
         />
         {/* <Route
           exact
