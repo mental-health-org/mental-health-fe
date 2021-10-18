3788204d9ab67516193b2fecea74317fbe085b45
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 13:06:46 -0600

    Organize components in containers folder

---
+++ src/containers/commentsContainer/CommentsContainer.tsx
@@ -0,0 +1,10 @@
+//this will render our indiv comment components
+//this will take in props of comments from parent - full question details component
+//this container will be a dumb component and recieve new props from parent component from response form on submit.
+const CommentsContainer = () => {
+  return (
+    <p>delete me</p>
+  )
+}
+
+export default CommentsContainer;
