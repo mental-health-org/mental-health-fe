f27b427a25f049e2e223aab7a6c6b577e6723c58
Matt Roden <mattroden92@gmail.com>
2021-10-18 16:42:44 -0600

    Initial setup

--- src/components/newQuestionForm/NewQuestionForm.tsx
+++ src/components/newQuestionForm/NewQuestionForm.tsx
@@ -1,1 +1,29 @@
-export {}
+import React from 'react'
+
+const NewQuestionForm:React.FC = () => {
+  return (
+    <section>
+      <div>Progress Bar Here</div>
+      <form>
+
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+
+        <button>Back</button>
+        <button>Submit</button>
+      </form>
+    </section>
+  )
+}
