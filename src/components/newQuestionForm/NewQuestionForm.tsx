042db13a3eda55993b372c32c67d722fbb824680
Matt Roden <mattroden92@gmail.com>
2021-10-18 17:02:23 -0600

    Pseudocode - begin adding state and empty method

--- src/components/newQuestionForm/NewQuestionForm.tsx
+++ src/components/newQuestionForm/NewQuestionForm.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 const NewQuestionForm:React.FC = () => {
+  // state: 
+    //questions - [{...}, {...}, ...]
+    //title - string
+    //context/body - string
+    // tags - string[]
+  
+  // new Question making function
+  // handleSubmit function
+
+    const [title, setTitle] = useState<string>('')
+    const [body, setBody] = useState<string>('')
+    const [tags, setTags] = useState<string[]>([])
+
+    const packageQuestion = () => {
+
+    }
+  
+    const handleSubmit = (event: React.FormEvent) => {
+
+    }
+
   return (
     <section>
       <div>Progress Bar Here</div>
