93d8685184d759114752badfe5023c1439104bd2
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 17:46:18 -0600

    Add skelaton for landing page

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -1,1 +1,20 @@
-export {}
+import TagSearchBar from '../../components/tagSearchBar/TagSearchBar';
+import QuestionsContainer from '../../containers/questionsContainer/QuestionsContainer'
+
+interface LandingPageProps {
+  tags: string[]
+}
+
+const LandingPage: React.FC<LandingPageProps> = (props) => {
+  //will pass props of tags to searchbar
+  return (
+    <div className="LandingPage">
+      LANDING PAGE
+      <button></button>
+      <TagSearchBar tags={props.tags}/>
+      <QuestionsContainer />
+    </div>
+  )
+}
+
+export default LandingPage;
