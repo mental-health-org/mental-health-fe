a6e07083063c357c44693eec4782dc8077ae8289
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 19:56:09 -0600

    Add tag interface

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -4,8 +4,13 @@ import { Route, Link, Switch } from "react-router-dom";
 import LandingPage from '../landingPage/LandingPage';
 import { useEffect, useState } from 'react';
 
+interface Tag {
+  id: number;
+  name: string;
+} 
+
 function App() {
-  const [tags, setTags] = useState<string[]>(['tag1', 'tag2', 'tag3']);
+  const [tags, setTags] = useState<Tag[]>([{id: 1, name: 'tag1'}, {id:2, name: 'tag2'}, {id:3, name: 'tag3'}]);
 
   useEffect(() => {
     //fetch all tags
