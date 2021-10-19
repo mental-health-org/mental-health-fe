91d17b65f66a4694d9436ec54723d08d78d7d045
Matt Roden <mattroden92@gmail.com>
2021-10-18 18:52:26 -0600

    Add questons state

--- src/components/newQuestionForm/NewQuestionForm.tsx
+++ src/components/newQuestionForm/NewQuestionForm.tsx
@@ -1,5 +1,7 @@
+import { stringify } from 'querystring';
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
+
 
 const NewQuestionForm:React.FC = () => {
   // state: 
@@ -11,11 +13,20 @@ const NewQuestionForm:React.FC = () => {
   // new Question making function
   // handleSubmit function
 
+  // Eventually: some sort of mapping fuction to dynamically create options 
+    //for the 'select' form element based off of a tags array. I assume the array will 
+    // come from the BE and be updated when a user makes a tag - not sure how to implement users making a tag yet 
+
     const [title, setTitle] = useState<string>('')
     const [body, setBody] = useState<string>('')
     const [tags, setTags] = useState<string[]>([])
+    const [questions, setQuestions] = useState<{id: number, title: string, body: string, tags: string}[]>([])
 
     const packageQuestion = () => {
+
+    }
+
+    const handleSelectorChange = () => {
 
     }
   
@@ -29,18 +40,34 @@ const NewQuestionForm:React.FC = () => {
       <form>
 
         <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+          <label>Question</label>
+          <input 
+            type="text" 
+            name='title'
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
         </div>
 
         <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+          <label>Provide More Context</label>
+          <input 
+            type="text"
+            name='body'
+            value={body}
+            onChange={event => setBody(event.target.value)} 
+          />
         </div>
 
         <div>
-          <label htmlFor=""></label>
-          <input type="text" />
+          <label>Tag Your Question</label>
+          <select name="tag-selector" multiple={true} onChange={handleSelectorChange}>
+            <option value="depression">Depression</option>
+            <option value="anxiety">Anxiety</option>
+            <option value="addiction">Addiction</option>
+            <option value="case-law">Case Law</option>
+            <option value="child-therapy">Child Therapy</option>
+          </select>
         </div>
 
       <Link to='/'>
