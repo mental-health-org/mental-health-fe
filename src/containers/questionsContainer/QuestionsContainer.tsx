f2e0e849473140bfb1129c168f810cfb17ffaf8a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 20:57:14 -0600

    Map through and create question cards

--- src/containers/questionsContainer/QuestionsContainer.tsx
+++ src/containers/questionsContainer/QuestionsContainer.tsx
@@ -1,9 +1,49 @@
-const QuestionsContainer = () => {
+import {useEffect, useState} from 'react';
+import { Question } from '../../interfaces';
+import QuestionCard from '../../components/questionCard/QuestionCard';
+
+
+interface QuestionContainerProps {
+  questions: Question[]
+}
+
+const QuestionsContainer: React.FC<QuestionContainerProps> = (props)=> {
+
+  const questionElements = props.questions.map(question=> <QuestionCard question={question}/>)
+
   return (
     <div className="QuestionsContainer">
-      QUESTIONS CONTAINER
+      {questionElements}
     </div>
   )
 }
 
 export default QuestionsContainer;
+
+// { questions: {
+//    id: null,
+//    type: "questions",
+//    attributes: {
+//     question_count: 150,
+//     questions: [
+//      {
+//       id: 1,
+//       title: "Need Help",
+//       responses: 10
+//       tags: [addiction, depression]
+//      },
+//      {
+//        id: 2,
+//          title: "Legal question",
+//          responses: 15
+//       tags: [addiction, sadage]
+//      },
+//      {
+//       id: 3,
+//       title: "How do I?",
+//       responses: 20
+//       tags: [addiction]
+//      }]
+//     }
+//    }
+//   }
