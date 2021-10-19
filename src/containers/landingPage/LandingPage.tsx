fcc3fe4850db5bb61137bd52d194982b9c2a9733
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 19:56:09 -0600

    Add tag interface

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -1,16 +1,22 @@
 import TagSearchBar from '../../components/tagSearchBar/TagSearchBar';
 import QuestionsContainer from '../../containers/questionsContainer/QuestionsContainer'
 
+
+interface Tag {
+  id: number;
+  name: string;
+} 
+
 interface LandingPageProps {
-  tags: string[]
+  tags: Tag[]
 }
 
 const LandingPage: React.FC<LandingPageProps> = (props) => {
   //will pass props of tags to searchbar
   return (
     <div className="LandingPage">
-      LANDING PAGE
-      <button></button>
+      Search by Tag
+      <button>Ask a Question</button>
       <TagSearchBar tags={props.tags}/>
       <QuestionsContainer />
     </div>
