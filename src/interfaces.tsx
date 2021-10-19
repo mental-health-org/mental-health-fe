1eaf85e01883c68760c1748753a9694424f716f5
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 20:54:29 -0600

    Add types

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -1,1 +1,20 @@
-export {}
+//Landing Page types
+//some of this data might be shared
+export interface SearchResult {
+  text: string;
+  suggestions: Tag[];
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+}
+
+export interface Question {
+  id: number;
+  title: string;
+  responses: number;
+  tags: string[];
+}
+
+
