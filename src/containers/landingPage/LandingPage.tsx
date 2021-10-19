1791cbb0aa372008b21ac6ce25d7e8af9f9ba88f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-18 20:56:47 -0600

    Add api call to fetch all questions

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -1,11 +1,9 @@
 import TagSearchBar from '../../components/tagSearchBar/TagSearchBar';
 import QuestionsContainer from '../../containers/questionsContainer/QuestionsContainer'
-
-
-interface Tag {
-  id: number;
-  name: string;
-} 
+import {Tag} from '../../interfaces';
+import {Link} from 'react-router-dom';
+import { Question } from '../../interfaces';
+import {useEffect, useState} from 'react';
 
 interface LandingPageProps {
   tags: Tag[]
@@ -13,12 +11,48 @@ interface LandingPageProps {
 
 const LandingPage: React.FC<LandingPageProps> = (props) => {
   //will pass props of tags to searchbar
+  //will pass questions to questions container and search will get updateQuestions method as props to update parent landing page so it can give updated questions to container.
+
+  //we may want the option to save all questions to local storage so we don't have to fetch again after a user decides to go back to just view all posts.
+  // we may want an option to "view feed again after a user filtered by tag"
+
+  const [allQuestions, setAllQuestions] = useState<Question[]>([]);
+
+useEffect(() => {
+  setAllQuestions([
+         {
+          id: 1,
+          title: "Need Help",
+          responses: 10,
+          tags: ['addiction', 'depression']
+         },
+         {
+           id: 2,
+           title: "Legal question",
+           responses: 15,
+            tags: ['addiction', 'sadage']
+         },
+         {
+            id: 3,
+            title: "How do I?",
+            responses: 20,
+            tags: ['addiction']
+         }])
+}, [])
+
+  // const updateQuestions = (tag) => {
+  //   //will make a fetch request by tag and reset questions.
+  // }
+
   return (
     <div className="LandingPage">
+      <h2>LANDING PAGE</h2>
+      <Link to="/ask"><button>Ask a Question</button></Link>
+      <br/>
       Search by Tag
-      <button>Ask a Question</button>
+      {/* <TagSearchBar tags={props.tags} updateQuestions={updateQuestions}/> */}
       <TagSearchBar tags={props.tags}/>
-      <QuestionsContainer />
+      <QuestionsContainer questions={allQuestions}/>
     </div>
   )
 }
