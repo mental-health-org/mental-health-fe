8c17b327144147de9ce21bc8d423325a1aa1e8c5
Matt Roden <mattroden92@gmail.com>
2021-10-18 23:19:44 -0600

    Resolve rebaseing conflicts - askPage import

--- src/containers/askPage/AskPage.tsx
+++ src/containers/askPage/AskPage.tsx
@@ -1,1 +1,12 @@
-export {}
+import React from 'react'
+import NewQuestionForm from '../../components/newQuestionForm/NewQuestionForm'
+
+const AskPage = () => {
+  return (
+    <section>
+      <NewQuestionForm />
+    </section>
+  )
+};
+
+export default AskPage;
