761bcfc66b0fa5749fa94b5651cd365988f397f2
Matt Roden <mattroden92@gmail.com>
2021-10-19 22:36:40 -0600

    Pass changeIsSubmitted method down as prop - successful state change on submit

--- src/containers/askPage/AskPage.tsx
+++ src/containers/askPage/AskPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NewQuestionForm from '../../components/newQuestionForm/NewQuestionForm'
 
 const AskPage = () => {
+  const [isSubmitClicked, setIsSubmitClicked] = useState<boolean>(false)
+
+  const changeIsSubmittedToFalse = (): void => {
+    setIsSubmitClicked(true)
+  }
+
   return (
     <section>
-      <NewQuestionForm />
+      <NewQuestionForm changeIsSubmittedToFalse={changeIsSubmittedToFalse}/>
     </section>
   )
 };
 
 export default AskPage;
 
-// ToDo:
-
-// 
+// Todo: 
+// build out modal - mui basic defualt modal
+// Show submission modal after user clicks submit
+  // make state/method of isSubmittedTrue/setIsSubmittedTrue
+  // pass down setter function to form
+  // when submit is clicked in form, reset the value of isSubmittedTrue from F to T
+  // add in conditional rendering logic
