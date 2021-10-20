59a77c6106308722b8055d821586f4ab3db5ac02
Matt Roden <mattroden92@gmail.com>
2021-10-19 20:43:39 -0600

    Build initial skeleton

---
+++ src/containers/TagsContainer/TagsContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TagsContainer: React.FC = () => {
+
+  return (
+    <div className='question-tags-container'>
+      Tags go HERE
+    </div>
+  )
+}
+
+export default TagsContainer
