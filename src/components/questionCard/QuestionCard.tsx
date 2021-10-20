6679d51150d9c98bc895f35abea1b03dfd773a0a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-20 11:36:03 -0600

    Pass delete method as props

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -2,6 +2,7 @@ import { Question } from '../../interfaces';
 
 interface QuestionCardProps {
   question: Question
+  deleteQuestion: (id:number) => void
 }
 
 const QuestionCard: React.FC<QuestionCardProps> = (props) => {
@@ -10,6 +11,7 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
       <h2>CARD</h2>
       <p>TITLE: {props.question.title}</p>
       <p>RESPONSES: {props.question.responses}</p>
+      <button onClick={() => {props.deleteQuestion(props.question.id)}}>delete</button>
     </div>
   )
 }
