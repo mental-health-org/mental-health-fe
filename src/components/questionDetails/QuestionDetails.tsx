887344fb8500859710d577ebdc032c5fc7c2d517
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-20 18:27:27 -0600

    Add to be delete functionality in comments

--- src/components/questionDetails/QuestionDetails.tsx
+++ src/components/questionDetails/QuestionDetails.tsx
@@ -1,5 +1,14 @@
 //this on useEffect will pull from our url to see which question we are at at make fetch for the question it needs
 //child components: 
   //this will render comments container
+
+//NOTE: to delete a card --> user has to click on the card from landing page to go to the individual card. If that card belongs to them, they can delete the card.
+  //interface QuestionsDetailsProps: {
+  //   deleteQuestion: (id: number) => void;
+  // } 
+  // <button onClick={() => {props.deleteQuestion(props.question.id)}}>delete</button> */}
+      
+
+
   export {}
 
