2e65bb1bfd384140793f902035fa2bdd71ef4392
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-20 18:31:40 -0600

    Add userContext skelaton

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,27 @@
+// import {createContext, useState} from 'react';
+// const UserContext = createContext<UserContextInterface | null>(null);
+
+// interface UserContextInterface {
+//   name: string
+//   id: number
+// }
+
+// const UserContextProvider = ({}) => {
+//   const [user, setUser] = useState([]);
+//   setUser({name:'Stephanie', id:1})
+
+//   return (
+//     <UserContext.Provider
+//       value={{
+//         user
+//       }}
+//     >
+//       {children}
+//     </UserContext.Provider>
+//   );
+
+// }
+
+// export { UserContextProvider , UserContext };
+
+export {}
