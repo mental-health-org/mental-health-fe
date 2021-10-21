d118573feef83087dd4f7fb48a9c48cfc6f44a9a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-20 18:26:57 -0600

    Remove delete functionality

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -4,8 +4,6 @@ import { useHistory } from "react-router-dom";
 
 interface QuestionCardProps {
   question: Question
-  deleteQuestion: (id:number) => void
-
 }
 
 
@@ -14,11 +12,8 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
   let history = useHistory();
 
   const redirect = (id:number) => {
-    // history.push(`/question${id}`)
     console.log('in redirect')
     const url = `/question${id}`
-    // return <Redirect to={url} />
-    
     history.push(url)
   };
 
@@ -27,11 +22,6 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
       <h2>CARD</h2>
       <p>TITLE: {props.question.title}</p>
       <p>RESPONSES: {props.question.responses}</p>
-      {/* //only show delete if the question matches the users id. */}
-      {/* TO TO : move this to the view question details view route. */}
-      {/* A user will have to click on the card to have the ability to go in and delete it because that funcitonality will be available in the view question detailst view
-      <button onClick={() => {props.deleteQuestion(props.question.id)}}>delete</button> */}
-      
     </div>
 
   )
