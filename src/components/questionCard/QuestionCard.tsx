3985529b5db47daa6aa000781ba31f9c3cdae1ad
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-20 18:05:07 -0600

    Add redirect for qustion cards

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -1,4 +1,6 @@
 import { Question } from '../../interfaces';
+import {Link, Redirect} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 interface QuestionCardProps {
   question: Question
@@ -6,9 +8,22 @@ interface QuestionCardProps {
 
 }
 
+
+
 const QuestionCard: React.FC<QuestionCardProps> = (props) => {
+  let history = useHistory();
+
+  const redirect = (id:number) => {
+    // history.push(`/question${id}`)
+    console.log('in redirect')
+    const url = `/question${id}`
+    // return <Redirect to={url} />
+    
+    history.push(url)
+  };
+
   return (
-    <div className="QuestionCard" key={props.question.id}>
+    <div className="QuestionCard" key={props.question.id} onClick={() => redirect(props.question.id)}>
       <h2>CARD</h2>
       <p>TITLE: {props.question.title}</p>
       <p>RESPONSES: {props.question.responses}</p>
@@ -18,6 +33,7 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
       <button onClick={() => {props.deleteQuestion(props.question.id)}}>delete</button> */}
       
     </div>
+
   )
 }
 
