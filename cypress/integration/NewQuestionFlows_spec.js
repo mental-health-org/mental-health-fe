e8e27e4acf2ab1ae62b9df6e7098a2a817a7f18f
Matt Roden <mattroden92@gmail.com>
2021-10-21 11:24:46 -0600

    Comment out testing code

--- cypress/integration/NewQuestionFlows_spec.js
+++ cypress/integration/NewQuestionFlows_spec.js
@@ -1,33 +1,33 @@
-const { cyan } = require("@mui/material/colors")
+// const { cyan } = require("@mui/material/colors")
 
-beforeEach(() => {
-  cy.visit('http://localhost:3000/#/ask')
-});
+// beforeEach(() => {
+//   cy.visit('http://localhost:3000/#/ask')
+// });
 
-describe('User flows and functionaloty for asking a new question', () => {
+// describe('User flows and functionaloty for asking a new question', () => {
 
-  it('should confirm true to be true', () => {
-    expect(true).to.equal(true)
-  });
+//   it('should confirm true to be true', () => {
+//     expect(true).to.equal(true)
+//   });
 
-  it('should visit the correct URL', () => {
-    cy.url().should('include', '/ask')
-  })
+//   it('should visit the correct URL', () => {
+//     cy.url().should('include', '/ask')
+//   })
 
-  it('should render a form that includes the correct elements', () => {
+//   it('should render a form that includes the correct elements', () => {
 
-  })
+//   })
 
-  it('Should render two buttons, back and submit', () => {
+//   it('Should render two buttons, back and submit', () => {
     
-  })
+//   })
 
-  it('Should allow the user to go back to the home page when clicking the back button', () => {
+//   it('Should allow the user to go back to the home page when clicking the back button', () => {
 
-  })
+//   })
 
-  it('should allow the user to input a tag and see it after clicking the add tag button', () => {
+//   it('should allow the user to input a tag and see it after clicking the add tag button', () => {
 
-  })
+//   })
 
-}); // end describe block
+// }); // end describe block
