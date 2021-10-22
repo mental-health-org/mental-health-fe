95f856e210775402127f843de7a3f74dc93ac367
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-22 05:23:02 -0600

    Add api calls

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,6 +2,18 @@ export const fetchAllQuestions = () => {
   return fetch('https://developer-mental-health-org.herokuapp.com/api/v1/questions').then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
+export const fetchAllTags = () => {
+  return fetch('https://developer-mental-health-org.herokuapp.com/api/v1/tags').then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
+}
+
+export const fetchQuestionsByTag = () => {
+  const keyword = 'addiction';
+  return fetch(`https://developer-mental-health-org.herokuapp.com/api/v1/tag/${keyword}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
+}
+
+
+
+
 
 // api/v1/questions  - GET, POST
 // api/v1/questions/:id - GET
