2219946d3b0cb80a147d091030227e5d6ea95e4a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-23 11:50:55 -0600

    Add QuestionDetails component structure

--- src/components/questionDetails/QuestionDetails.tsx
+++ src/components/questionDetails/QuestionDetails.tsx
@@ -1,2 +1,19 @@
-export {}
+import {QuestionDetailsObject} from '../../interfaces';
+
+interface QuestionDetailsProps {
+  questionDetails: QuestionDetailsObject;
+}
+
+const QuestionDetails: React.FC<QuestionDetailsProps> = (props) => {
+  return (
+    <div className='QuestionDetails'>
+    <p>{props.questionDetails.id}</p>
+    <p>{props.questionDetails.title}</p>
+
+  </div>
+  )
+  
+}
+
+export default QuestionDetails;
 
