9a09c86e7ae19e77780006a0e9c9239658b5234b
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-23 13:20:59 -0600

    Add comment to commentContainer

--- src/containers/commentsContainer/CommentsContainer.tsx
+++ src/containers/commentsContainer/CommentsContainer.tsx
@@ -1,9 +1,14 @@
 //this will render our indiv comment components
 //this will take in props of comments from parent - full question details component
 //this container will be a dumb component and recieve new props from parent component from response form on submit.
+import Comment from '../../components/comment/Comment'
+
 const CommentsContainer = () => {
   return (
-    <p>delete me</p>
+    <div className='CommentsContainer'>
+      <Comment />
+    </div>
+  
   )
 }
 
