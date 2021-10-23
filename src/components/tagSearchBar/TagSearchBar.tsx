85273e44a4e8626882734b89bb33c50174585239
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-22 18:23:47 -0600

    Add MUI dropdown filter functionality

--- src/components/tagSearchBar/TagSearchBar.tsx
+++ src/components/tagSearchBar/TagSearchBar.tsx
@@ -1,96 +1,52 @@
-//TO DO: choose react icon.
-// import { FaArrowDown } from "@react-icons/all-files/fa/FaArrowDown";
-import { useState } from "react";
-import { Tag, SearchResult } from "../../interfaces";
-import SearchIcon from '@mui/icons-material/Search';
+import { useEffect } from "react";
+import { Tag } from "../../interfaces";
 import "./tagSearchBar.scss";
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 interface TagSearchBarProps {
   tags: Tag[];
-  updateQuestions: (tag: string) => void;
+  updateQuestions: (tag: string ) => void;
+}
+
+const TagSearchBar: React.FC<TagSearchBarProps> = (props) => {
+  const options = props.tags.map(tag => tag.name)
+  const [value, setValue] = React.useState<any>(options[0]);
+  const [inputValue, setInputValue] = React.useState('');
+
+  const updateQuestionsByTag = (newInputValue: string) => {
+    props.updateQuestions(value)
+    setInputValue(newInputValue)
+  }
+
+  useEffect(()=> {
+    props.updateQuestions(value)
+  }, [inputValue, props, value])
+
+  return (
+    <div>
+      <div>{`${value !== null ? `Search Results for '${value}'` : 'null'}`}</div>
+      <div>{`inputValue: '${inputValue}'`}</div>
+      <br />
+      <Autocomplete
+        value={value}
+        onChange={(event: any, newValue: string | null) => {
+          setValue(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+          // updateQuestionsByTag(inputValue)
+        }}
+        id="controllable-states-demo"
+        options={options}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="search by tag" />}
+      />
+    </div>
+  );
 }
 
 
-const TagSearchBar: React.FC<TagSearchBarProps> = (props) => {
-  const [search, setSearch] = useState<SearchResult>({
-    text: "",
-    suggestions: [],
-  });
-  const [isComponentVisible, setIsComponentVisible] = useState<boolean>(true);
-
-  const onTextChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    let suggestions: Tag[] = [];
-    if (value.length > 0) {
-      const regex = new RegExp(`^${value}`, "i");
-      suggestions = props.tags.sort().filter((v: Tag) => regex.test(v.name));
-    }
-    setIsComponentVisible(true);
-    setSearch({ suggestions, text: value });
-  };
-
-  const searchByKeywordOrTag = () => {
-    console.log('search by keyword')
-    //RIGHT HERE IS WHERE THE REQUEST WOULD BE MADE TO GRAB NEW QUESTIONS BY TAG
-    //delete and replace with the right method.
-    props.updateQuestions('test')
-    setSearch({text:"", suggestions: []})
-  }
-
-  const makeSuggestionSelected = (
-    e: React.MouseEvent<HTMLButtonElement>,
-    value: Tag
-  ) => {
-    e.preventDefault();
-    setIsComponentVisible(false);
-    setSearch({
-      text: value.name,
-      suggestions: [],
-    });
-    searchByKeywordOrTag()
-  };
-
-  // what is this below?
-  const { suggestions } = search;
-
-  return (
-    <div className="TagSearchBar">
-      <div
-    //do not comment out onClick. // this is causing things to break.
-        onClick={() => setIsComponentVisible(false)}
-      />
-      <div>
-        <h3>Find a Topic By Tag</h3>
-        <span className="search">
-          <input
-            id="input"
-            autoComplete="off"
-            value={search.text}
-            onChange={onTextChanged}
-            type={"text"}
-          />
-        {/* To do: if searching by something other than tag need to have a controlled input */}
-          <button className="tag-search-button"><SearchIcon onClick={() => searchByKeywordOrTag()}/></button>
-        </span>
-      </div>
-      {suggestions.length > 0 && isComponentVisible && (
-        <div>
-          {suggestions.map((item: Tag) => (
-            <div>
-              <button
-                key={item.id}
-                onClick={(e) => makeSuggestionSelected(e, item)}
-              >
-                {item.name}
-              </button>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
 export default TagSearchBar;
-
-
