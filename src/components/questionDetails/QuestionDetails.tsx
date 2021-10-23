717ade724e680b3aec23f86777e7b658c2d11891
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-23 12:13:33 -0600

    Build out component to match UI

--- src/components/questionDetails/QuestionDetails.tsx
+++ src/components/questionDetails/QuestionDetails.tsx
@@ -6,18 +6,31 @@ interface QuestionDetailsProps {
   deleteQuestion: (id: number) => void;
 }
 
+// TO DO: if the post matches the users post -- only then show delete my post button
+//TO DO: add modal --> are you sure youd like to delete your post?
 
 const QuestionDetails: React.FC<QuestionDetailsProps> = (props) => {
   return (
-    <div className='QuestionDetails'>
-    <p>{props.questionDetails.id}</p>
-    <p>{props.questionDetails.title}</p>
+    <div className='QuestionDetails' key={props.questionDetails.id}>
+      <h2>Questions and Answers</h2>
+      <p>{props.questionDetails.created_at}</p>
+      <h3>{props.questionDetails.title}</h3>
+      <p>{props.questionDetails.body}</p>
+      <p>Upvote: {props.questionDetails.upvote}</p>
+      <p>DownVote: {props.questionDetails.downvote}</p>
     <Button
         variant="outlined"
         className="read-button"
         onClick={() => {props.deleteQuestion(props.questionDetails.id)}}
       >
         Delete My Post
+      </Button>
+      <Button
+        variant="outlined"
+        className="read-button"
+        onClick={() => console.log('add a comment')}
+      >
+        Add Comment
       </Button>
 
   </div>
