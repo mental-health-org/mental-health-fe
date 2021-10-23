f86f1c5e74a666fed5f4fe4833965375655f037f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-23 12:06:26 -0600

    Add conditional rendering for slow data

--- src/containers/viewQuestionPage/ViewQuestionPage.tsx
+++ src/containers/viewQuestionPage/ViewQuestionPage.tsx
@@ -3,34 +3,35 @@
   //this will render comments container
 
 //NOTE: to delete a card --> user has to click on the card from landing page to go to the individual card. If that card belongs to them, they can delete the card.
-  //interface QuestionsDetailsProps: {
-  //   deleteQuestion: (id: number) => void;
-  // } 
-  // <button onClick={() => {props.deleteQuestion(props.question.id)}}>delete</button> */}
+
   import { useParams } from 'react-router-dom';
   import { useEffect, useState } from 'react';
   import {fetchQuestionByID} from '../../utils/util';
   import QuestionDetails from '../../components/questionDetails/QuestionDetails'
-  import {QuestionDetailsObject} from '../../interfaces'
+
 
   interface RouteParams {
-      id: string
+      id: string;
   }
 
-  const ViewQuestionPage: React.FC = () => {
+  interface ViewQuestionPageProps {
+    deleteQuestion: (id: number) => void;
+  }
+
+  const ViewQuestionPage: React.FC<ViewQuestionPageProps> = (props) => {
     const params = useParams<RouteParams>();
     const [details, setDetails] = useState<any>(null)
 
     console.log("props--->", params.id)
     // const { id }= useParams();
+  
 
     useEffect(() => {
-      fetchQuestionByID(params.id).then(data => console.log(data))
       fetchQuestionByID(params.id).then(data => setDetails(data))
     }, [params.id])
 
     return <div className="ViewQuestionPage">
-      <QuestionDetails questionDetails={details}/>
+      {details && <QuestionDetails questionDetails={details} deleteQuestion={props.deleteQuestion}/>}
     </div>
   }
       
