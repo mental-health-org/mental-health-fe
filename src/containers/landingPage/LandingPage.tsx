40110b975893b01a2f0f84f501f729fcaa0631b0
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-23 18:42:48 -0600

    Conditionally render when data is fetched

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -16,18 +16,21 @@ const LandingPage: React.FC<LandingPageProps> = (props) => {
   //will pass props of tags to searchbar
   //will pass questions to questions container and search will get updateQuestions method as props to update parent landing page so it can give updated questions to container.
 
+  console.log("landing page is being rerendered!")
 
   return (
     <div className="LandingPage">
-      <h1>Landing Page</h1>
-      <h2>Have a Question?</h2>
+      <h2 className="landingPage--h2">Have a Question?</h2>
       <Link className="ask-link" to="/ask"><button
       className="ask--btn">Ask</button></Link>
       <br/>
       
       {/* <TagSearchBar tags={props.tags} updateQuestions={updateQuestions}/> */}
-      <TagSearchBar tags={props.tags} updateQuestions={props.updateQuestions}/>
-      <QuestionsContainer questions={props.allQuestions} />
+
+      {props.tags.length > 0 && <TagSearchBar tags={props.tags} updateQuestions={props.updateQuestions}/>}
+      
+      {props.allQuestions.length > 0 && <QuestionsContainer questions={props.allQuestions} />}
+      {props.allQuestions.length < 0 && <p>Sorry, no questions for this tag. Add one here! (insert link) </p>}
     </div>
   )
 }
