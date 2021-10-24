6dc505555129407e16ab55e4268d1e57ada4b3d4
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 15:57:07 -0600

    Add footer

---
+++ src/containers/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <h1> HeLP Network (Helping and Law Professionals)</h1>
+      <h4> Help. Learn. Connect. Grow.</h4>
+      <p> Mission: connecting expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p>
+    </div>
+    
+  )
+}
+
+export default Footer;
