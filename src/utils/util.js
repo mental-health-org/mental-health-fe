086a0b9d4f6ec7c51a8b83e55e01038e76c5fc0f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 11:10:31 -0600

    Add fetchQuestionsByKeyword

--- src/utils/util.js
+++ src/utils/util.js
@@ -7,7 +7,6 @@ export const fetchAllTags = () => {
 }
 
 export const fetchQuestionsByTag = (tag) => {
-
   return fetch(` https://developer-mental-health-org.herokuapp.com/api/v1/filter/questions/?tags=${tag}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
@@ -15,9 +14,13 @@ export const fetchQuestionByID = (id) => {
   return fetch(`https://developer-mental-health-org.herokuapp.com/api/v1/questions/${id}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
+export const fetchQuestionsByKeyword = (keyword) => {
+  return fetch(` https://developer-mental-health-org.herokuapp.com/api/v1/search/questions/?search=${keyword}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
+}
 
 
 
+///api/v1/search/questions/?search=    (KEYWORD)
 
 // api/v1/questions  - GET, POST
 // api/v1/questions/:id - GET
