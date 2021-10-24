6d0767655e7106f4b09495b7aa48022cfaa0f77a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 16:18:54 -0600

    Clean up format

--- src/utils/util.js
+++ src/utils/util.js
@@ -18,12 +18,3 @@ export const fetchQuestionsByKeyword = (keyword) => {
   return fetch(` https://developer-mental-health-org.herokuapp.com/api/v1/search/questions/?search=${keyword}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
-
-
-///api/v1/search/questions/?search=    (KEYWORD)
-
-// api/v1/questions  - GET, POST
-// api/v1/questions/:id - GET
-// api/v1/tags - GET
-// api/v1/tags/:id - GET
-// api/v1/posts/:id 
