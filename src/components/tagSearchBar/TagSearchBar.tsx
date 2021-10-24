c2a01b183d7fe37138625316dfdf42ae37135410
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 16:17:30 -0600

    Clean up format

--- src/components/tagSearchBar/TagSearchBar.tsx
+++ src/components/tagSearchBar/TagSearchBar.tsx
@@ -1,49 +1,39 @@
-import { useEffect } from "react";
 import { Tag } from "../../interfaces";
 import "./tagSearchBar.scss";
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import TagsContainer from "../../containers/TagsContainer/TagsContainer";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 interface TagSearchBarProps {
   tags: Tag[];
-  updateQuestions: (type: string, query: string ) => void;
+  updateQuestions: (type: string, query: string) => void;
 }
 
 const TagSearchBar: React.FC<TagSearchBarProps> = (props) => {
-  // const options = props.tags.map(tag => tag.name)
-  console.log("tag search bar is being rerendered")
-  const options = props.tags
+  const options = props.tags;
   const [value, setValue] = React.useState<any>(options[0]);
-  const [inputValue, setInputValue] = React.useState('');
-  const [isSearchSet, setSearch] = React.useState<boolean>(false)
-  // const [searchResultsText, setSearchResultsText] = React.useState('All search Results')
+  const [inputValue, setInputValue] = React.useState("");
+  const [isSearchSet, setSearch] = React.useState<boolean>(false);
 
   const updateQuestionsByTag = (newValue: any) => {
- 
-      props.updateQuestions('tag', newValue)
-      setSearch(true)
-      if(newValue === null) {
-
-      }
-  }
-
+    props.updateQuestions("tag", newValue);
+    setSearch(true);
+  };
 
   return (
-    <div className='TagSearchBar'>
+    <div className="TagSearchBar">
       <div>
-        {value === null && 'All search results'}
+        {value === null && "All search results"}
         {!isSearchSet && `All search results`}
-        {(value !== null && isSearchSet) && `Search Results for '${value}'`}
-         </div>
+        {value !== null && isSearchSet && `Search Results for '${value}'`}
+      </div>
       <br />
-      <div className='autocomplete--container'>
+      <div className="autocomplete--container">
         <Autocomplete
           value={value}
           onChange={(event: any, newValue: string | null) => {
             setValue(newValue);
-            updateQuestionsByTag(newValue)
+            updateQuestionsByTag(newValue);
           }}
           inputValue={inputValue}
           onInputChange={(event, newInputValue) => {
@@ -52,14 +42,13 @@ const TagSearchBar: React.FC<TagSearchBarProps> = (props) => {
           id="controllable-states-demo"
           options={options}
           sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="search by tag" />}
+          renderInput={(params) => (
+            <TextField {...params} label="search by tag" />
+          )}
         />
       </div>
     </div>
   );
-}
-
+};
 
 export default TagSearchBar;
-
-
