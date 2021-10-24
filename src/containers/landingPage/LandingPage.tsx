174cc267ca3859c0c592a3a2f7911e4bba08734d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 16:19:02 -0600

    Clean up format

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -1,42 +1,41 @@
-import TagSearchBar from '../../components/tagSearchBar/TagSearchBar';
-import QuestionsContainer from '../../containers/questionsContainer/QuestionsContainer'
-import {Tag} from '../../interfaces';
-import {Link} from 'react-router-dom';
-import { Question } from '../../interfaces';
-import Button from '@mui/material/Button';
-import './landingPage.scss';
-import KeywordSearchInput from '../../components/keywordSearchInput/KeywordSearchInput'
+import TagSearchBar from "../../components/tagSearchBar/TagSearchBar";
+import QuestionsContainer from "../../containers/questionsContainer/QuestionsContainer";
+import { Tag } from "../../interfaces";
+import { Link } from "react-router-dom";
+import { Question } from "../../interfaces";
+import "./landingPage.scss";
+import KeywordSearchInput from "../../components/keywordSearchInput/KeywordSearchInput";
 
 interface LandingPageProps {
   tags: Tag[];
-  updateQuestions: (type: string, query: string ) => void;
+  updateQuestions: (type: string, query: string) => void;
   allQuestions: Question[];
   isEmptySearch: boolean;
 }
 
 const LandingPage: React.FC<LandingPageProps> = (props) => {
-  //will pass props of tags to searchbar
-  //will pass questions to questions container and search will get updateQuestions method as props to update parent landing page so it can give updated questions to container.
-
-  console.log("landing page is being rerendered!")
-
   return (
     <div className="LandingPage">
       <h2 className="landingPage--h2">Have a Question?</h2>
-      <Link className="ask-link" to="/ask"><button
-      className="ask--btn">Ask</button></Link>
-      <br/>
-      
-      {/* <TagSearchBar tags={props.tags} updateQuestions={updateQuestions}/> */}
-
-      {props.tags.length > 0 && <TagSearchBar tags={props.tags} updateQuestions={props.updateQuestions}/>}
-
+      <Link className="ask-link" to="/ask">
+        <button className="ask--btn">Ask</button>
+      </Link>
+      <br />
+      {props.tags.length > 0 && (
+        <TagSearchBar
+          tags={props.tags}
+          updateQuestions={props.updateQuestions}
+        />
+      )}
       <KeywordSearchInput updateQuestions={props.updateQuestions} />
-      
-      {props.allQuestions.length > 0 && <QuestionsContainer questions={props.allQuestions} />}
-      {props.isEmptySearch && <p>Sorry, no questions are available for this query. </p>}
+      {props.allQuestions.length > 0 && (
+        <QuestionsContainer questions={props.allQuestions} />
+      )}
+      {props.isEmptySearch && (
+        <p>Sorry, no questions are available for this query. </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
