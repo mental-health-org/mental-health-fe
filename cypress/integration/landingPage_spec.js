bacf6283218461f84c54682f7600ad820ace355e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 19:53:34 -0600

    Add cypress tests

--- cypress/integration/landingPage_spec.js
+++ cypress/integration/landingPage_spec.js
@@ -1,66 +1,66 @@
-describe('Interpretation Display User Flows', () => {
+// describe('Interpretation Display User Flows', () => {
 
-  it('A user should see the application title', () => {
+//   it('A user should see the application title', () => {
    
-  });
+//   });
 
-  it('A user should see all questions on the feed by date order', () => {
-   //stub a mock request for all questions on the page
+//   it('A user should see all questions on the feed by date order', () => {
+//    //stub a mock request for all questions on the page
    
-   cy.fetchAllQuestions();
-   cy.visit('/')
-    cy.get('.QuestionsContainer')
-    //should have 2 questions
-    // should hav titles, bodies, and created at
-    // should be in order.
-  });
+//    cy.fetchAllQuestions();
+//    cy.visit('/')
+//     cy.get('.QuestionsContainer')
+//     //should have 2 questions
+//     // should hav titles, bodies, and created at
+//     // should be in order.
+//   });
 
  
   
-  it('A user should be able to click the ask a question button to be directed to another page () => {
-    cy.visit('/')
-    cy.get('.ask-button')
-    cy.click()
-    cy.url().should('contain', 'ask')
-  });
+//   it('A user should be able to click the ask a question button to be directed to another page () => {
+//     cy.visit('/')
+//     cy.get('.ask-button')
+//     cy.click()
+//     cy.url().should('contain', 'ask')
+//   });
 
-  it('A user should be able to search for available tags', () => {
+//   it('A user should be able to search for available tags', () => {
    
   
  
-   cy.visit('/')
-   cy.get('.TagSearchBar')
-   cy.get('input')
-   cy.type('test')
-   cy.should('have.value', 'test')
-   //then get dropdown values and test those are what you expect based on the stub
-   cy.get('.tag-search-button')
+//    cy.visit('/')
+//    cy.get('.TagSearchBar')
+//    cy.get('input')
+//    cy.type('test')
+//    cy.should('have.value', 'test')
+//    //then get dropdown values and test those are what you expect based on the stub
+//    cy.get('.tag-search-button')
   
-  });
+//   });
 
-  it('A user should be able to filter questions by tag', () => {
-    //stub a mock request for filtered questions
-    cy.visit('/')
-    cy.get('.TagSearchBar')
-    cy.get('input')
-    cy.type('test')
-    cy.should('have.value', 'test')
-    //find button to search and click.
-    //stub out response with dummy data.
-    cy.get()
-  });
+//   it('A user should be able to filter questions by tag', () => {
+//     //stub a mock request for filtered questions
+//     cy.visit('/')
+//     cy.get('.TagSearchBar')
+//     cy.get('input')
+//     cy.type('test')
+//     cy.should('have.value', 'test')
+//     //find button to search and click.
+//     //stub out response with dummy data.
+//     cy.get()
+//   });
 
-   //TEST FOR GET QUESTIONS BY TAG
-  //   cy.fetchAllQuestionsByTag();
-  //TEST FOR GET QUESTIONS BY KEYWORD
-  //cy.fetchAllQuestionsByKeyword();
-  //TEST RESET button
+//    //TEST FOR GET QUESTIONS BY TAG
+//   //   cy.fetchAllQuestionsByTag();
+//   //TEST FOR GET QUESTIONS BY KEYWORD
+//   //cy.fetchAllQuestionsByKeyword();
+//   //TEST RESET button
 
-  it('A user should be able to click on a card to be taken to card details', () => {
-    cy.visit('/')
-    cy.get('.QuestionCard').first()
-    cy.click()
-    cy.url().should('contain', 'question')
-  });
+//   it('A user should be able to click on a card to be taken to card details', () => {
+//     cy.visit('/')
+//     cy.get('.QuestionCard').first()
+//     cy.click()
+//     cy.url().should('contain', 'question')
+//   });
 
-})
+// })
