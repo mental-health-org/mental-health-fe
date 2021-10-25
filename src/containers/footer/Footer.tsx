f9bc5a10a33ca7e1ea38afd06efcf22781776b53
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-25 08:42:38 -0600

    Add className for styling

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -3,9 +3,9 @@ import './footer.scss';
 const Footer = () => {
   return (
     <div className="Footer">
-      <h1> HeLP Network (Helping and Law Professionals)</h1>
-      <h4> Help. Learn. Connect. Grow.</h4>
-      <p> Mission: connecting expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p>
+      <h1 className="footer--font footer--title"> HeLP Network (Helping and Law Professionals)</h1>
+      <h4 className="footer--font"> Help. Learn. Connect. Grow.</h4>
+      <p className="footer--font"> Mission: connecting expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p>
     </div>
   )
 }
