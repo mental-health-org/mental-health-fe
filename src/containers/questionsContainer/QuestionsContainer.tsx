c058f902079c33228cd3b1f1701d10b687988ef0
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-24 18:32:22 -0600

    Sort questions by date

--- src/containers/questionsContainer/QuestionsContainer.tsx
+++ src/containers/questionsContainer/QuestionsContainer.tsx
@@ -2,11 +2,13 @@ import { Question } from '../../interfaces';
 import QuestionCard from '../../components/questionCard/QuestionCard';
 import './questionsContainer.scss';
 
+
 interface QuestionContainerProps {
   questions: Question[]
 }
 
 const QuestionsContainer: React.FC<QuestionContainerProps> = (props)=> {
+  props.questions.sort((a,b) => b['id'] - a['id'])
   const questionElements = props.questions.map(question=> <QuestionCard question={question} key={question.id}/>)
 
   return (
