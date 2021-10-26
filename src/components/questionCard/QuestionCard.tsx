8accc81106f48f039c5d6b4437568cad7ad3afa6
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-26 11:20:20 -0600

    Conditionally render response count

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -18,14 +18,19 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
     history.push(url);
   };
 
+
+
   const dateCreated = props.question.updated_at.slice(0,10)
 
   return (
     // <div className="QuestionCard" key={props.question.id} onClick={() => redirect(props.question.id)}>
     <div className="QuestionCard" key={props.question.id}>
-      <span className="user--span"><PersonIcon/><p className="detail person-title">counselor</p></span>
+      {(<span className="user--span"><PersonIcon/><p className="detail person-title">{props.question.user && props.question.user.title}</p></span>)}
       <h2 className="questionCard--h2">{props.question.title}</h2>
       <p>{props.question.body.slice(0, 60)} ...</p>
+      {props.question['response_count'] > 1 && <p> {props.question['response_count'] } responses </p>}
+      {props.question['response_count'] === 1 && <p> {props.question['response_count'] } response </p>}
+      {props.question['response_count'] === 0 && <p> {props.question['response_count'] } responses </p>}
       <p className="detail">Post Date: {dateCreated}</p>
       {/* <p>{props.question.responses} responses</p> */}
 
