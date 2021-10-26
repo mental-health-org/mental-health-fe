0b7f15a0cd7601c6b3e567399c9be94905f8a2c4
Matt Roden <mattroden92@gmail.com>
2021-10-26 14:09:39 -0600

    Add in code for component

--- src/components/DownVote/DownVote.tsx
+++ src/components/DownVote/DownVote.tsx
@@ -1,1 +1,20 @@
-export {}
+import React from 'react'
+import {QuestionDetailsObject} from '../../interfaces';
+import '../../styles/DownVote.scss'
+
+interface DownVoteProps{
+  questionDownVote: (event: React.FormEvent) => void;
+  questionDetails: QuestionDetailsObject;
+}
+
+const DownVote: React.FC<DownVoteProps> = ({ questionDownVote, questionDetails }) => {
+  return (
+    <button 
+      className='DownVote--button'
+      onClick={(event) => questionDownVote(event)}
+      >DownVote: {questionDetails.downvotes}
+    </button>
+  )
+}
+
+export default DownVote;
