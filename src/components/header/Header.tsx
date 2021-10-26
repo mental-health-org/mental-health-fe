0c2989719cc06cc543a710f01042500b4af76b6f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-25 19:58:59 -0600

    Add header to conditionally render color pop in landing page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../styles/Header.scss'
+import mentalHealthLogo from '../../images/mental_health_logo1.png';
 
 
 interface HeaderProps {
@@ -10,7 +11,9 @@ const Header: React.FC<HeaderProps> = ({ headerTitle }) => {
   return (
     <header className='Header--container'>
       <div className='BottomBorder--border'>
-        <h1 className='HeaderTitle--h1'>{headerTitle}</h1>
+       <img src={mentalHealthLogo} alt="head outline with lotus flower" className="logo--landing-page"></img>
+      {headerTitle !== 'HeLP Network' && <h1 className='HeaderTitle--h1'>{headerTitle}</h1>} 
+        {headerTitle === 'HeLP Network' && <h1 className='HeaderTitle--h1'>H<span className="letter-standout">e</span>LP Network</h1>}
       </div>
     </header>
   )
