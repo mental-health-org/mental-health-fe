b71d1132409ef5214adbf7f8acbe393bb50d927e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-26 19:05:21 -0600

    Clean commented out code

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './comment.scss'
+import '../../styles/comment.scss'
 import { Response } from '../../interfaces';
 import PersonIcon from '@mui/icons-material/Person';
 
@@ -9,43 +9,18 @@ interface CommentProps {
 
 const Comment: React.FC<CommentProps> = ({ responseText }) => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  // const [value, setValue] = useState('');
-  
-
-//   const onChange = () => {
-//    setValue(value);
-//  };
-
-//  const handleSave = () => {
-//   setIsEditing(false)
-//  }
-
-//   const handleClick = () => {
-//     setIsEditing(true)
-//   }
 
   return (
     <div className='Comment--container'>
     {!isEditing && (
       <>
         {(<span className="user--span"><PersonIcon/><p className="detail person-title"> {responseText.user && <p>{responseText.user.title}</p> }
-        {/* {responseText.user && <p>{responseText.user.username}</p> } */}
         </p></span>)}
         <p>{responseText['created_at'].slice(0,10)}</p>
         <p className='CommentText--p'>{responseText.body}</p>
-        {/* <button onClick={() => handleClick()}>EDIT THIS COMMENT</button> */}
       </>
       )
     }
-
-    {/* {isEditing && 
-    (
-      <>
-    <input type="text" value={value} onChange={onChange} placeholder={value}/>
-    <button onClick={() => handleSave() }>Save Comment</button>
-    </>)
-    } */}
-  
   </div>
     )
 
