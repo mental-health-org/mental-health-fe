e4886e9745c5179d6ad41136e4e25875232de392
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-26 19:27:00 -0600

    Remove code comments

--- src/utils/util.js
+++ src/utils/util.js
@@ -14,15 +14,6 @@ export const fetchQuestionByID = (id) => {
   return fetch(`https://developer-mental-health-org.herokuapp.com/api/v1/questions/${id}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
-///POST NEW COMMENT AND QUESTION WILL BOTH NEED THIS.
-// const [user, setUser] = useState(
-//   {
-//       "id": 1,
-//       "username": "TestUser",
-//       "title": null,
-//       "created_at": "2021-10-21T19:13:02.707669Z",
-//       "updated_at": "2021-10-21T19:13:02.707686Z"
-//   })
 
 export const postNewComment = (newComment) => {
   return fetch('https://developer-mental-health-org.herokuapp.com/api/v1/responses/', {
@@ -39,9 +30,7 @@ export const fetchQuestionsByKeyword = (keyword) => {
   return fetch(` https://developer-mental-health-org.herokuapp.com/api/v1/search/questions/?search=${keyword}`).then((res) => res.ok ? res.json() : console.log("something went wrong")).catch(err => err)
 }
 
-// api/v1/questions/  - GET, POST
-// api/v1/questions/:id - GET
-// api/v1/tags - GET
-// api/v1/tags/:id - GET
-// api/v1/posts/:id 
+
+
+
 
