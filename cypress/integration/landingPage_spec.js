4a7562750442d5ff026e92c6af054fa513e92636
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-26 18:23:24 -0600

    Add search input and tag dropdown tests

--- cypress/integration/landingPage_spec.js
+++ cypress/integration/landingPage_spec.js
@@ -1,21 +1,22 @@
 describe('Interpretation Display User Flows', () => {
 
   it('A user should see the header and footer', () => {
-  cy.visit('/')
+    cy.fetchAllQuestions()
+    cy.visit('http://localhost:3000/')
    cy.get('.HeaderTitle--h1')
    cy.should('contain', 'HeLP Network')
 
    cy.get('.Footer')
-   cy.get('footer--title')
-   cy.should('contain', 'HeLp Network')
+   cy.get('.footer--title')
+   cy.should('contain', 'HeLP Network')
    cy.get('.mission--text')
    cy.should('contain', 'Our mission is to connect expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other.')
   });
 
   it('A user should see all questions on the feed by date order', () => {
    
-   cy.fetchAllQuestions();
-  //  cy.visit('/')
+    cy.fetchAllQuestions();
+    cy.visit('http://localhost:3000/')
     cy.get('.QuestionsContainer')
     cy.get('.QuestionCard').first()
     cy.get('h2')
@@ -29,7 +30,6 @@ describe('Interpretation Display User Flows', () => {
     cy.get('.detail')
     cy.should('contain', '2021-10-21')
 
-
     cy.get('.QuestionsContainer')
     cy.get('.QuestionCard').last()
     cy.get('h2')
@@ -38,6 +38,7 @@ describe('Interpretation Display User Flows', () => {
     cy.should('contain', 'Some body of information')
     cy.get('.response-count')
     cy.should('contain', '5')
+    cy.get('button')
     cy.should('contain', 'READ')
     cy.get('.detail')
     cy.should('contain', '2021-9-21')
@@ -45,51 +46,60 @@ describe('Interpretation Display User Flows', () => {
   });
 
  
-  
   it('A user should be able to click the ask a question button to be directed to another page', () => {
-    cy.visit('/')
-    cy.get('.ask-button')
-    cy.click()
+    cy.get('.ask--btn').click()
     cy.url().should('contain', 'ask')
   });
 
+
+  //TO DO - come back to testing this!
   it('A user should be able to search for available tags', () => {
-   
-   cy.visit('/')
-   cy.get('.TagSearchBar')
-   cy.get('input')
-   cy.type('tag1')
-   cy.should('have.value', 'tag1')
+    cy.fetchAllQuestions()
+    cy.fetchAllTags()
+  cy.visit('http://localhost:3000/')
+  cy.fetchAllQuestionsByTag('Tag1')
+  cy.get('.MuiOutlinedInput-root').type('First Tag').type('{enter}').type('{enter}')
+  cy.get('.MuiOutlinedInput-root').type('{enter}')
+
+  //add an id to the drop down ---
+  
+  //.should("have.value", "Beef");
+  // cy.findByText('First Tag').click()
+  
    //then get dropdown values and test those are what you expect based on the stub
-   cy.get('.tag-search-button')
-   cy.fetchAllQuestionsByTag()
-   cy.click()
+  //  cy.fetchAllQuestionsByTag('Tag1')
   
   });
 
   it('A user should be able to filter questions by tag', () => {
     //stub a mock request for filtered questions
-    cy.visit('/')
-    cy.get('.TagSearchBar')
-    cy.get('input')
-    cy.type('test')
+    cy.fetchAllQuestions()
+    cy.visit('http://localhost:3000/')
+    cy.get('.filterByTitle--input').get('input').type('test')
     cy.should('have.value', 'test')
+    
     //find button to search and click.
     //stub out response with dummy data.
-    cy.get()
+    // cy.get()
+
+// cy.get('.TagSearchBar')
+//    cy.get('.filterByTitle--input').last()
+//    .type('Tag1')
+//    cy.should('have.value', 'Tag1')
+//    //then get dropdown values and test those are what you expect based on the stub
+   cy.fetchAllQuestionsByKeyword()
+   cy.get('.search--btn')
+  .click()
   });
 
-   //TEST FOR GET QUESTIONS BY TAG
-  //   cy.fetchAllQuestionsByTag();
-  //TEST FOR GET QUESTIONS BY KEYWORD
-  //cy.fetchAllQuestionsByKeyword();
-  //TEST RESET button
 
-  it('A user should be able to click on a card to be taken to card details', () => {
-    cy.visit('/')
-    cy.get('.QuestionCard').first()
-    cy.click()
-    cy.url().should('contain', 'question')
-  });
+//   //TEST RESET button
+
+//   it('A user should be able to click on a card to be taken to card details', () => {
+//     cy.visit('/')
+//     cy.get('.QuestionCard').first()
+//     cy.click()
+//     cy.url().should('contain', 'question')
+//   });
 
 })
