77903920cbd859e5ba2c5e83b1370e76ca886cd8
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-27 06:41:50 -0600

    Add commands for new tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -99,18 +99,23 @@ Cypress.Commands.add('fetchAllQuestionsByKeyword', () => {
     body: 
         [
           {
-              "id": 2,
+              "id": 3,
               "title": "test",
-              "body": "Questions about anxiety",
+              "body": "test about anxiety",
               "upvote": 0,
               "downvote": 0,
               "tagging": ['tag1'],
-              "created_at": "2021-10-21T21:20:57.757299Z",
-              "updated_at": "2021-10-21T21:20:57.757318Z"
+              "created_at": "2021-4-21T21:20:57.757299Z",
+              "updated_at": "2021-3-21T21:20:57.757318Z",
+              "response_count": 7,
+              "user": {
+                username: 'testUser2',
+                title: 'testTitle2'
+              }
           },
           {
-              "id": 3,
-              "title": "Keyword1 sort of title",
+              "id": 2,
+              "title": "test sort of title",
               "body": "Some body of information",
               "upvote": 0,
               "downvote": 0,
@@ -118,13 +123,58 @@ Cypress.Commands.add('fetchAllQuestionsByKeyword', () => {
                   "tag1",
                   "tag2"
               ],
-              "created_at": "2021-10-22T00:24:35.360530Z",
-              "updated_at": "2021-10-22T00:24:35.360572Z"
+              "created_at": "2021-4-22T00:24:35.360530Z",
+              "updated_at": "2021-4-22T00:24:35.360572Z",
+              "response_count": 11,
+              "user": {
+                username: 'testUser2',
+                title: 'testTitle2'
+              }
           },
       ]
   });
 });
-// /////////////////
+
+//https://developer-mental-health-org.herokuapp.com/api/v1/questions/${id}
+Cypress.Commands.add('fetchQuestionsByID', () => {
+  cy.intercept('GET', `https://developer-mental-health-org.herokuapp.com/api/v1/questions/`, {
+    statusCode: 200,
+    body: [
+      {
+          "id": 1,
+          "title": "Need Help",
+          "body": "Questions about anxiety",
+          "upvotes": 0,
+          "downvotes": 0,
+          "tagging": [],
+          "created_at": "2021-10-21T21:20:57.757299Z",
+          "updated_at": "2021-10-21T21:20:57.757318Z",
+          "response_count": 2,
+          "user": {
+            username: 'testUser1',
+            title: 'testTitle1'
+          }
+        }
+        ]
+  })
+
+  cy.intercept('GET', 'https://developer-mental-health-org.herokuapp.com/api/v1/questions/1', {
+    statusCode: 200,
+    body: {
+        body: "JUST TEST TEXT",
+        created_at: "2021-10-26T20:07:24.809094Z",
+        downvotes: 0,
+        id: 113,
+        responses: [],
+        tagging: ['Law', 'Confidentiality', 'Duty To Warn'],
+        title: "Duty to Warn -- DHS Clarification",
+        updated_at: "2021-10-26T20:07:24.809118Z",
+        upvotes: 1,
+        user: {username: 'tester', title: 'tester'}
+      }
+  });
+
+});
 
 // Commands for NewQuestionForm
 Cypress.Commands.add('askNewQuestion', () => {
@@ -167,4 +217,4 @@ Cypress.Commands.add('askNewQuestion', () => {
 }); // end new question command
 
 
-//Commands for Commenting
+
