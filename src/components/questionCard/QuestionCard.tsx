7b86316ed6e0fc1651ead502826a924d5812d5c6
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-28 08:10:07 -0600

    Fix conditional rendering bug

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -23,7 +23,7 @@ const QuestionCard: React.FC<QuestionCardProps> = (props) => {
           <p className="detail person-title">{props.question.user && props.question.user.title}</p>
         </span>)}
       <h2 className="questionCard--h2">{props.question.title}</h2>
-      <p className="body">{props.question.body.slice(0, 60)} ...</p>
+      {props.question.body && <p className="body">{props.question.body.slice(0, 60)} ...</p>}
       {props.question['response_count'] > 1 && <p className="response-count"> {props.question['response_count'] } responses </p>}
       {props.question['response_count'] === 1 && <p className="response-count"> {props.question['response_count'] } response </p>}
       {props.question['response_count'] === 0 && <p className="response-count"> {props.question['response_count'] } responses </p>}
