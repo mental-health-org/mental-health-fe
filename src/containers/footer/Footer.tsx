645e890302fdc8236bb3252a6b34678c390a7e83
Matt Roden <mattroden92@gmail.com>
2021-10-28 18:30:50 -0600

    Add footer text back in

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         {/* <p className="footer--font">(Helping and Law Professionals)</p>  */}
       </div>
       <h4 className="footer--font"> Help. Learn. Connect. Grow.</h4>
-      {/* <p className="footer--font mission--text"> Our mission is to connect expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p> */}
+      <p className="footer--font mission--text"> Our mission is to connect expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p>
     </div>
   )
 }
