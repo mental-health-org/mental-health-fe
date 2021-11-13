a0b6534101db1e126aeca981c7f321ec910e1fe4
Matt Roden <mattroden92@gmail.com>
2021-11-13 14:31:10 -0700

    Build base, import, and render Delete component button

--- src/components/Delete/Delete.tsx
+++ src/components/Delete/Delete.tsx
@@ -1,1 +1,9 @@
-export {}
+import React from 'react'
+
+const Delete:React.FC = () => {
+  return (
+    <button>Delete</button>
+  )
+}
+
+export default Delete
--- src/containers/UserActionsBox/UserActionsBox.tsx
+++ src/containers/UserActionsBox/UserActionsBox.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../../styles/UserActionsBox.scss'
+import Delete from '../../components/Delete/Delete'
 
 const UserActionsBox: React.FC = () => {
   return (
     <div className='UserActionsBox--container'>
-      UserActionsBox
+      <Delete />
     </div>
   )
 }
