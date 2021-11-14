a46418d1f109b25b1736f7f35a8902d946253d90
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-14 13:58:40 -0700

    Import css for loader

--- src/components/questionDetails/QuestionDetails.tsx
+++ src/components/questionDetails/QuestionDetails.tsx
@@ -9,6 +9,7 @@ import PersonIcon from '@mui/icons-material/Person';
 import UpVote from '../UpVote/UpVote';
 import DownVote from '../DownVote/DownVote';
 import React from 'react';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
 const loader = <Loader
