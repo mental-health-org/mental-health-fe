656cb83d4313d1c778a1803849c7d098056f3e32
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-15 19:47:55 -0700

    Add notes for backend code

--- src/utils/util.js
+++ src/utils/util.js
@@ -72,7 +72,9 @@ export const removeResponse = (id) => {
 
 // STEP 1:
 //woudld this request be sent to the backend and be sent back to the frontend.
-export const requestLinkedInAuth = (href) => {
+//frontend would send the code found in the url to the backend.
+// backend would make this request to linkedin with this code to get back the access code. (we would as a FE need to make a get request that included the code in the body) 
+export const requestLinkedInAuth = (codeFromURL) => {
   return fetch(`https://www.linkedin.com/oauth/v2/accessToken`, {
     method: 'POST',
     headers: {
@@ -81,7 +83,7 @@ export const requestLinkedInAuth = (href) => {
     },
     body: JSON.stringify({
       'grant_type': 'authorization_code',
-      'code': `${href}`,
+      'code': `${codeFromURL}`,
       'client_id': '86n4l10lucphsb',
       'client_secret': 'kvQNvGy7foUTrZpi',
       'redirect_uri': 'https%3A%2F%2Fmental-health-fe.herokuapp.com'
@@ -90,10 +92,16 @@ export const requestLinkedInAuth = (href) => {
   .then(response => response.json())
   .catch(err => err)
 }
+//backend would then get the response from linkedin which the access token and send that in a second request to linkedin
+
+// {
+//   "access_token": "AQU280QIiP8DD_GNFzS_GVnHVeHf7eJDTXWYQfdZUFfceaKqSXhgLlXhPb7g7QFpBzCxgngtLmcNGcVWqJbkXnfsRyAOrV2Su1vJIp3z1BlQRLeWee0vbBc8RaZ9qEOeDYqcnse_IEBAsQHdonTmtISBIlNEcZ9md1dQjxbrWrJUjVE70R8sYqE5dxSpuwgmiUuN9875b0Ad_-zadtB7KEBVxnUYyf42KzsfbaH9I0oYhPzdxoKLi62HgPTjtLxPBPO9Fh1frt_R9fd0Jnw3CyP3mHD7-9J0QNnKNZ8hrpUzb2t-2PWO1B5OwwSLJ_9zGi3ymQicX-ZqwuBq9IhzmLJ5hgTAAQ",
+//   "expires_in": 5183999
+// }
 
 //STEP 2: PASS BEARER TOKEN:
 export const getLinkedInUserData = (bearerToken) => {
-  return fetch(`https://www.linkedin.com/oauth/v2/accessToken`, {
+  return fetch(`https://api.linkedin.com/v2/me`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
