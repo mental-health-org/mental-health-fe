ed78de2bb1195b8caf9fc5f8e39f21fc4280c7d9
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-17 14:51:47 -0700

    Import userContext

--- src/containers/UserActionsBox/UserActionsBox.tsx
+++ src/containers/UserActionsBox/UserActionsBox.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import '../../styles/UserActionsBox.scss'
-import Delete from '../../components/Delete/Delete'
+import Delete from '../../components/Delete/Delete';
+// import ReportProblemIcon from '@mui/icons-material/ReportProblem';
+import {flagQuestion, flagComment} from '../../utils/util'
+import FlagModal from '../../components/flagModal/FlagModal'
+import { UserContext } from '../../contexts/UserContext';
+import {useContext} from 'react';
+
+/// now that I used a cast I have to say if its empty then account for this condition since you bypassed tyepscript.
+// you are here******
+
 
 interface UserActonsBoxProps {
   delete: (id: number) => void;
   id: number;
   update: () => void;
   updateDeleteStatus: () => void;
+  type: string;
 }
 
 const UserActionsBox: React.FC<UserActonsBoxProps> = (props) => {
+  const { userData } = useContext(UserContext);
+  console.log("here is my context here", userData, useContext(UserContext))
 
   const handleDeleteClick = (event: React.FormEvent) => {
     props.delete(props.id)
@@ -17,11 +29,28 @@ const UserActionsBox: React.FC<UserActonsBoxProps> = (props) => {
     props.updateDeleteStatus()
   }
 
+const packagePost = () => {
+//   {
+//     "post" : id // is this the id of the post
+//     "user" : userData.id
+//     "comment" : "bad post" /// this needs to be grabbed from the modal!
+// }
+
+// {
+//   "response" : id
+//   "user" : userData.id
+//   "comment" : "bad post"
+// }
+}
+
+
+
   return (
     <div className='UserActionsBox--container'>
       <Delete 
         handleClick={handleDeleteClick}
       />
+      <FlagModal handleFlagClick={handleFlagClick} type={props.type}/>
     </div>
   )
 }
