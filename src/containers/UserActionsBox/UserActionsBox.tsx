f4ec5e97c2e99f466a6a6a4024335f3277d6e341
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-18 08:57:13 -0700

    Remove code comments

--- src/containers/UserActionsBox/UserActionsBox.tsx
+++ src/containers/UserActionsBox/UserActionsBox.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import '../../styles/UserActionsBox.scss'
 import Delete from '../../components/Delete/Delete';
-// import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import {flagQuestion, flagComment} from '../../utils/util'
 import FlagModal from '../../components/flagModal/FlagModal'
 import { UserContext } from '../../contexts/UserContext';
 import {useContext} from 'react';
-
-/// now that I used a cast I have to say if its empty then account for this condition since you bypassed tyepscript.
 
 interface UserActonsBoxProps {
   delete: (id: number) => void;
@@ -29,9 +26,9 @@ const UserActionsBox: React.FC<UserActonsBoxProps> = (props) => {
 const packagePost = (type: string, comment: string) => {
 if(type === "question") {
   return {
-    "post": props.id, // is this the id of the post
+    "post": props.id, 
     "user": userData.id,
-    "comment": comment /// this needs to be grabbed from the modal!
+    "comment": comment 
   }
 } else if(type === "comment")
   return {
@@ -41,23 +38,17 @@ if(type === "question") {
   }
 }
 
-  // I'm not sure this is a form event!
   const handleFlagClick = (event: React.FormEvent, comment: string ) => {
     event.preventDefault()
     const postObject = packagePost("question", comment)
     if (props.type === "question") {
-      
-      //flagQuestion
       flagQuestion(postObject).then(data => console.log(data))
       .catch(err => console.log("err", err))
     } else if (props.type === "comment") {
-      //flagComment
-      
       flagComment(postObject).then(data => console.log(data))
       .catch(err => console.log("err", err))
     }
   }
-
 
   return (
     <div className='UserActionsBox--container'>
