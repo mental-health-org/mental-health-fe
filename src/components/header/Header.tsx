c33237d7c4f8db6000442dd2eb5fa50e1a8c628d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-24 14:33:30 -0700

    Add logout button to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../../styles/Header.scss'
 import mentalHealthLogo from '../../images/mental_health_logo1.png';
+import  LogoutButton from '../logoutButton/LogoutButton'
 
 interface HeaderProps {
   headerTitle: string;
+  removeToken: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ headerTitle}) => {
+const Header: React.FC<HeaderProps> = ({ headerTitle, removeToken}) => {
   return (
     <header className='Header--container'>
+      <LogoutButton removeToken={removeToken} />
       <div className='BottomBorder--border heading--container'>
        <img src={mentalHealthLogo} alt="head outline with lotus flower" className="logo--landing-page"></img>
       {headerTitle !== 'HeLP Network' && <h1 className='HeaderTitle--h1'>{headerTitle}</h1>} 
