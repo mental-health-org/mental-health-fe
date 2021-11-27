2ea9fdb4e0b25e755b335aa2ea5d9edf767fce25
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-26 19:26:56 -0700

    Add removeCookie method to remove userData

--- src/components/logoutButton/LogoutButton.tsx
+++ src/components/logoutButton/LogoutButton.tsx
@@ -1,16 +1,21 @@
-import './logoutButton.scss'
+import './logoutButton.scss';
+import { useCookies } from "react-cookie";
 
 interface LogoutButtonProps {
   // removeToken: () => void;
 }
 
 const LogoutButton: React.FC<LogoutButtonProps>  = (props) => {
+  const [cookies, setCookie, removeCookie] = useCookies();
 
   const logout = () => {
     console.log("I am clicked")
     // clear session storage here and clear token from app state.
-    localStorage.removeItem("currentUser")
-    // props.removeToken()
+    //localStorage.removeItem("currentUser")
+    removeCookie('currentUser')
+    console.log("cookies after logout", cookies);
+   
+    
     //update below before deployment:
     // window.location.href = 'https://mental-health-fe.herokuapp.com/'
     // http://localhost:3000/
