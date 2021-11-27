ca42e9975ec3086681587a0504ebf6457876271c
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-27 16:09:50 -0700

    Add conditional rendering for logout button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import '../../styles/Header.scss'
 import mentalHealthLogo from '../../images/mental_health_logo1.png';
 import  LogoutButton from '../logoutButton/LogoutButton'
+import { UserContext } from '../../contexts/UserContext';
 
 interface HeaderProps {
   headerTitle: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ headerTitle}) => {
+
+  // const { userData } = useContext(UserContext);
+  
   return (
     <header className='Header--container'>
-      <LogoutButton />
+      {localStorage.getItem("currentUser") && <LogoutButton />}
       <div className='BottomBorder--border heading--container'>
         <img src={mentalHealthLogo} alt="head outline with lotus flower" className="logo--landing-page"></img>
       {headerTitle !== 'HeLP Network' && <h1 className='HeaderTitle--h1'>{headerTitle}</h1>} 
