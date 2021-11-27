3c25c02e2d3f2528944868099c0d3331c0534de9
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-27 11:26:09 -0700

    Clean up component

--- src/components/logoutButton/LogoutButton.tsx
+++ src/components/logoutButton/LogoutButton.tsx
@@ -1,22 +1,9 @@
 import './logoutButton.scss';
-import { useCookies } from "react-cookie";
-
-interface LogoutButtonProps {
-  // removeToken: () => void;
-}
 
 const LogoutButton: React.FC<LogoutButtonProps>  = (props) => {
-  const [cookies, setCookie, removeCookie] = useCookies();
 
   const logout = () => {
-    console.log("I am clicked")
-    // clear session storage here and clear token from app state.
     localStorage.removeItem("currentUser")
-    // removeCookie('currentUser')
-    // console.log("cookies after logout", cookies);
-   
-    
-    //update below before deployment:
     // window.location.href = 'https://mental-health-fe.herokuapp.com/'
     // http://localhost:3000/
     window.location.href = 'http://localhost:3000/'
