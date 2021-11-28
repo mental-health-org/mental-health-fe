55d6d5644bca2f583064b0c1a87e80ae1a9b60cb
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-11-28 08:57:26 -0700

    Remove code comments

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <div className="Footer">
       <div> 
         <h1 className="footer--font footer--title"> HeLP Network </h1> 
-        {/* <p className="footer--font">(Helping and Law Professionals)</p>  */}
       </div>
       <h4 className="footer--font"> Help. Learn. Connect. Grow.</h4>
       <p className="footer--font mission--text"> Our mission is to connect expertise across speciality areas of public helping professions to ask and find questions to important legal and ethical concerns central to helping to support our communities and each other. </p>
